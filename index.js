93c8ab7c66ecf2e32cea741300b70b4afc687cc0
Danish Mujahid <danish.mujahid@airtimerewards.com>
2023-07-27 15:36:23 +0100

    added dockerfile

--- index.js
+++ index.js
@@ -1,20 +1,35 @@
 const express = require('express');
+const { Op } = require('sequelize');
 const app = express();
 const { Joke } = require('./db');
 
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 
 app.get('/jokes', async (req, res, next) => {
   try {
-    // TODO - filter the jokes by tags and content
-    const jokes = [];
+    let where = {};
+    if (req.query.tags) {
+      where = {
+        tags: {
+          [Op.substring]: req.query.tags,
+        },
+      };
+    } else if (req.query.content) {
+      where = {
+        joke: {
+          [Op.substring]: req.query.content,
+        },
+      };
+    }
+    const jokes = await Joke.findAll({ where });
     res.send(jokes);
   } catch (error) {
     console.error(error);
-    next(error)
+    next(error);
   }
 });
 
-// we export the app, not listening in here, so that we can run tests
+
+
 module.exports = app;
